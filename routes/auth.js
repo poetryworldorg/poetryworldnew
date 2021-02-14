67630eb63c9fc295d4a3682e7c12aceb0785dc66
yeshu varshney <yeshuvarshney@yeshus-MacBook-Pro.local>
2021-02-14 15:47:10 +0530

    Request change

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const e = require('express');
 var express = require('express');
 //var app = express.app();
 var app = express();  
@@ -9,12 +10,12 @@ app.get('/', function(req, res, next) {
 
 app.post('/signup', function(req, res, next) {
   signup(req, res, next);
-  res.send('respond with a resource');
+  
 });
 
 app.post('/login', function(req, res, next) {
   login(req, res, next);
-  res.send('respond with a resource');
+ 
 });
 
 function login(req, res, next){
@@ -23,17 +24,22 @@ function login(req, res, next){
     "email":data.email,
     "password":data.password,
   }
-  if(data.email||data.password,data.gender){
-
+  if(data.email||data.password){
+    return res.status(205).json({ message: "Input validation fail",statusMessage:"205" });
   }
+  else{
+  
   new Promise((resolve, reject) => { 
     if (Math.random() > 0) { 
         resolve('Hello, I am positive number!'); 
+        return res.status(200).json({ message: "Login2" });
     } 
     reject(new Error('I failed some times')); 
 })
+  }
 console.log("RequestData: ", requestData); 
 }
+
 function signup(req, res, next){
   var data = req.body; 
   var requestData={
@@ -44,20 +50,24 @@ function signup(req, res, next){
     "address":data.address,
     "dob":data.dob
   }
-  if(data.email||data.password,data.gender){
-
+  if(data.email||data.password||data.gender||data.name){
+    return res.status(205).json({ message: "Input validation fail",statusMessage:"205" });
   }
+  else{
+  
   new Promise((resolve, reject) => { 
     if (Math.random() > 0) { 
         resolve('Hello, I am positive number!'); 
+        return res.status(200).json({ message: "Login2" });
     } 
     reject(new Error('I failed some times')); 
 })
+  }
   console.log("RequestData: ", requestData); 
   
 }
-app.listen(PORT, function(err){ 
-  if (err) console.log(err); 
-  console.log("Server listening on PORT", PORT); 
-});
+// app.listen(PORT, function(err){ 
+//   if (err) console.log(err); 
+//   console.log("Server listening on PORT", PORT); 
+// });
 module.exports = app;
